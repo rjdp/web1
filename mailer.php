fd02871380417877ebe2961ff62521757af4e60b
Rajdeep <rjdp9736@gmail.com>
2015-09-29 03:43:01 +0530

    commit before demo upload

--- mailer.php
+++ mailer.php
@@ -1,43 +1,84 @@
 <?php
-    // My modifications to mailer script from:
-    // http://blog.teamtreehouse.com/create-ajax-contact-form
-    // Added input sanitizing to prevent injection
+$to = "rjdp9736@gmail.com, buggydebugger@gmail.com";
+$subject = "HTML email";
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
-        $phone = trim($_POST["phone"]);
+$name=$_POST["name"];
+$phone=$_POST["phone"];
+$email=$_POST["email"];
+$message=$_POST["message"];
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR empty($phone) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+// Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR empty($phone) empty($email)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
             exit;
         }
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "rjdp9736@gmail.com";
 
-        // Set the email subject.
-        $subject = "New contact from $name";
+$email_content = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body style='font-family: arial,sans-serif;'>
+<div style='max-width:600px;margin-left:auto;margin-right:auto;background-color:#1295c9;color:white;padding:5px'>
+<div style='text-align:center;padding:5px 0;background-color: white;'>
+<a href='http://divyaeyeclinicpune.com' style='text-decoration:none;color:#fff;/* background-color: white; */' target='_blank'><img width='130' src='https://openmerchantaccount.com/img/plainlogobrandsv2out.png' style='width: 35%;margin-left: -6%;
+'></a><br>
+ <b style='color:black;'>Query Details!</b></div>
+ <div style='background-color:#f4f4f4;padding:10px;margin-top:6px'>
+ 
 
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Phone: $phone\n\n";
-        $email_content .= "Message:\n$message\n";
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
+ <div style='background-color:white;padding:10px;border:1px solid #ddd;margin-top:10px;border-bottom:4px solid #ddd'>
+ <div style='color:#1295c9;margin-top:3px'><b>Name</b>&nbsp;</div>
+ <hr color='#ddd' size='1'>
+ <div style='margin-top:5px;display:inline-block;color:#333;width:187px'>
+ <b>". $name ."</b>
+ </div>
+ </div>
 
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
+  <div style='background-color:white;padding:10px;border:1px solid #ddd;margin-top:10px;border-bottom:4px solid #ddd'>
+ <div style='color:#1295c9;margin-top:3px'><b>Email-Id</b>&nbsp;</div>
+ <hr color='#ddd' size='1'>
+ <div style='margin-top:5px;display:inline-block;color:#333;width:187px'>
+ <b>". $email ."</b>
+ </div>
+ </div>
+
+ <div style='background-color:white;padding:10px;border:1px solid #ddd;margin-top:10px;border-bottom:4px solid #ddd'>
+ <div style='color:#1295c9;margin-top:3px'><b>Phone</b>&nbsp;</div>
+ <hr color='#ddd' size='1'>
+ <div style='margin-top:5px;display:inline-block;color:#333;width:187px'>
+ <b>". $phone ."</b>
+ </div>
+ </div>
+
+  <div style='background-color:white;padding:10px;border:1px solid #ddd;margin-top:10px;border-bottom:4px solid #ddd'>
+ <div style='color:#1295c9;margin-top:3px'><b>Message</b>&nbsp;</div>
+ <hr color='#ddd' size='1'>
+ <div style='margin-top:5px;display:inline-block;color:#333;width:187px'>
+ <b>". $message ."</b>
+ </div>
+ </div>
+
+</div>
+</div>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <rjdp9737@gmail.com>' . "\r\n";
+$headers .= 'Cc: sharmarajdeep4@gmail.com' . "\r\n";
+
+
+if (mail($to,$subject,$email_content,$headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
             echo "Thank You! Your message has been sent.";
@@ -47,10 +88,5 @@
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
 
 ?>
